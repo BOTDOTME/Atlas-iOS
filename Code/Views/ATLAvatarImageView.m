610d8099339e63030a7c214504af0607e1c2b347
Ernesto Rivera <rivera.ernesto@gmail.com>
2017-05-16 12:54:04 -0400

    Revert "Improve ATLAvatarImageView’s initialsLabel handling"

--- Code/Views/ATLAvatarImageView.m
+++ Code/Views/ATLAvatarImageView.m
@@ -108,30 +108,21 @@ NSString *const ATLAvatarImageViewAccessibilityLabel = @"ATLAvatarImageViewAcces
     [self.downloadTask cancel];
 }
 
-- (void)setImage:(UIImage *)image
-{
-    super.image = image;
-    
-    if (image)
-    {
-        self.initialsLabel.text = nil;
-    }
-}
-
 - (void)setAvatarItem:(id<ATLAvatarItem>)avatarItem
 {
     self.image = nil;
     
-    if (avatarItem.avatarInitials) {
-        self.initialsLabel.text = avatarItem.avatarInitials;
-    }
-    
-    if (avatarItem.avatarImageURL) {
+    if ([avatarItem avatarImageURL]) {
+        self.initialsLabel.text = nil;
         [self loadAvatarImageWithURL:[avatarItem avatarImageURL]];
     } else if (avatarItem.avatarImage) {
+        self.initialsLabel.text = nil;
         self.image = avatarItem.avatarImage;
     }
     
+    if (self.image == nil && avatarItem.avatarInitials) {
+        self.initialsLabel.text = avatarItem.avatarInitials;
+    }
     _avatarItem = avatarItem;
 }
 
@@ -206,6 +197,7 @@ NSString *const ATLAvatarImageViewAccessibilityLabel = @"ATLAvatarImageViewAcces
         self.alpha = 0.0;
     } completion:^(BOOL finished) {
         [UIView animateWithDuration:0.5 animations:^{
+            self.initialsLabel.text = nil;
             self.image = image;
             self.alpha = 1.0;
         }];
